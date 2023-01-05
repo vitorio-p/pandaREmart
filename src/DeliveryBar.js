31a8923b532825841d577865f91329ec069961f4
vitorio-p <vitorio.pan@gmail.com>
2023-01-05 21:19:00 +0800

    updated DeliveryBar.js

--- src/DeliveryBar.js
+++ src/DeliveryBar.js
@@ -4,10 +4,20 @@ export default class DeliveryBar extends React.Component {
   render() {
     return (
       <React.Fragment>
-          <div className="container-fluid border">
-            <div className="text-center">DELIVERING TO:</div>
-            <div className="text-center">New Address: <b>Singapore 158748</b></div>
+        <div className="row">
+          <div className="container-fluid border col">
+            <div className="text-center pinkFont">DELIVERING TO:</div>
+            <div className="text-center">
+              <b className="pinkFont">Home</b> <b>Singapore 158748</b>
+            </div>
           </div>
+          <div className="container-fluid border col">
+            <div className="text-center">WHEN:</div>
+            <div className="text-center">
+              <b>ASAP</b>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
