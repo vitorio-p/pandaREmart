a3576b499e7b03492e709008f8484576c1fbb9d1
vitorio-p <vitorio.pan@gmail.com>
2023-01-26 11:19:49 +0800

    Added CheckoutBar and CheckoutPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from 'react-router-dom';
 import StoreFront from "./pages/StoreFront";
 import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<StoreFront/>} />
       <Route exact path="/cart" element={<CartPage/>} />
+      <Route exact path="/checkout" element={<CheckoutPage/>} />
     </Routes>
   );
 }
---
+++ src/pages/ItemPage.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import { useParams, Link, generatePath, useHistory } from "react-router-dom";
+
+// export default function ItemPage() {
+//   const { id } = useParams();
+
+//   const handleProceed = (e) => {
+//     history.pushState(generatePath("/item/:id", { id }));
+//   };
+// }
