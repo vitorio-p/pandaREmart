64f5d2ac51180bda6ccaba89ff1709bccb59deec
vitorio-p <vitorio.pan@gmail.com>
2023-02-26 20:41:31 +0800

    remove unused svg

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as UserSVG } from "../svg/user.svg";
-import { ReactComponent as LikedSVG } from "../svg/liked.svg";
+// import { ReactComponent as LikedSVG } from "../svg/liked.svg";
 import { ReactComponent as CartSVG } from "../svg/cart.svg";
 import { ReactComponent as FoodpandaLogoSVG } from "../svg/foodpanda-logo-plain.svg";
 
@@ -9,10 +9,10 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar container-fluid border px-2">
+        <nav className="navbar container-fluid border px-2 position-relative">
           <UserSVG />
-          <FoodpandaLogoSVG />
-          <LikedSVG />
+          <FoodpandaLogoSVG className="position-absolute start-50 translate-middle-x"/>
+          {/* <LikedSVG /> */}
           <Link to="/cart">
             <CartSVG />
           </Link>
