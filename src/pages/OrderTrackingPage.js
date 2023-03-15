e460ad8cb95dd7255aba5e4c3254ea66c8cc141e
vitorio-p <vitorio.pan@gmail.com>
2023-03-15 16:12:33 +0800

    FE - Checkout Page UI

---
+++ src/pages/OrderTrackingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+export default function OrderTrackingPage() {
+  return (
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
+  );
+}
