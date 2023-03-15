26682ee03d8b46fa4f684ce8543f44a53c4ab080
vitorio-p <vitorio.pan@gmail.com>
2023-03-15 12:24:38 +0800

    FE - Add basic cart functionality

--- src/components/DeliveryBar.js
+++ src/components/DeliveryBar.js
@@ -4,19 +4,17 @@ export default class DeliveryBar extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container-fluid">
+        <div className="container-fluid text-center">
           <div className="row">
-            <div className="col border">
-              <div className="text-center">DELIVERING TO:</div>
-              <div className="text-center">
+            <div className="col padding border">
+              <div>DELIVERING TO:</div>
+              <div>
                 <b className="pinkFont">Home</b> <b>Singapore 158748</b>
               </div>
             </div>
-            <div className="col border">
-              <div className="text-center">WHEN:</div>
-              <div className="text-center">
-                <b>ASAP</b>
-              </div>
+            <div className="col padding border">
+              <div>WHEN:</div>
+              <b>ASAP</b>
             </div>
           </div>
         </div>
