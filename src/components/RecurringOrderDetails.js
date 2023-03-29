bef1ff5dc2ddc09a18ff7938bf091fa3764fc55a
vitorio-p <vitorio.pan@gmail.com>
2023-03-29 09:49:12 +0800

    added cart details into component

--- src/components/RecurringOrderDetails.js
+++ src/components/RecurringOrderDetails.js
@@ -2,6 +2,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function RecurringOrderDetails() {
+  const state = {
+    items: [
+      {
+        id: 0,
+        name: "Dasoon Premium Fresh Egg 15S",
+        price: 5,
+        quantity: 1,
+      },
+      {
+        id: 1,
+        name: "Meiji Fresh Milk 2L",
+        price: 7,
+        quantity: 1,
+      },
+      {
+        id: 2,
+        name: "Gardenia Jumbo 600 Enriched White Bread",
+        price: 3,
+        quantity: 1,
+      },
+    ],
+    others: {
+      subtotal: 15,
+      deliveryFee: 2,
+      total: 17,
+    },
+  };
+
+  function renderItems() {
+    let jsx = [];
+    let id = 1;
+
+    for (let item of state.items) {
+      if (item.quantity !== 0) {
+        jsx.push(
+          <div className="cart-box padding" id={id}>
+            <div className="cart-details-item">
+              <div>{item.quantity} ×</div>
+              <div className="padding">{item.name}</div>
+              <div className="cart-item-price">S$ {item.price}</div>
+            </div>
+          </div>
+        );
+        id++;
+      }
+    }
+    return jsx;
+  }
+
   const cancelOrder = () => {
     const order = document.getElementById("order");
     order.remove();
@@ -16,8 +65,25 @@ export default function RecurringOrderDetails() {
 
   return (
     <React.Fragment>
-      <div className="card recurring-order-page">
+      <div className="card recurring-order-page ">
         <h3 className="padding">Order 1</h3>
+        <div className="card checkout-page orders">
+          <h3>Cart details</h3>
+          <div className="cart-content">{renderItems(state)}</div>
+          <div className="side-by-side">
+            <div>Subtotal</div>
+            <div>S$ {state.others.subtotal}</div>
+          </div>
+          <div className="side-by-side">
+            <div>Delivery fee</div>
+            <div>S$ {state.others.deliveryFee}</div>
+          </div>
+          <div className="side-by-side">
+            <div>Total</div>
+            <div>S$ {state.others.total}</div>
+          </div>
+        </div>
+
         <div className="rop-component">
           <table class="table table-striped">
             <thead>
